131b4911a886dc50869c636ed91dcbb03ce9fb13
akash sharma <mcakky93@gmail.com>
2020-11-26 16:00:15 +0530

    this is new file

--- a.py
+++ a.py
@@ -1,4 +1,5 @@
-l=["akash","Sharma","ankit","Nitin","Upender"]
+l=["Akash","Sharma","ankit","Nitin","Upender"]
 s="a"
-a=list(filter(lambda x: x[0]==s,l))
+s1="A"
+a=list(filter(lambda x: x if x[0]==s else x[0]==s1,l))
 print(a)
