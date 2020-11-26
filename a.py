4bdb808a6a22cf62a9def99cb53596a2677d4839
akash sharma <mcakky93@gmail.com>
2020-11-26 22:16:08 +0530

    chngae in thirsty

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 l=["Akash","Sharma","ankit","Nitin","Upender","Aman","Avinash","Pankaj"]
 s="a"
-s1="A"
+s1="S"
 a=list(filter(lambda x: x if x[0]==s else x[0]==s1,l))
 print(a)
+print(" This is the change in thirsty branch")
