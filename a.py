cdd5835fa17b337204c057123a8469d42132a050
akash sharma <mcakky93@gmail.com>
2020-11-26 16:11:49 +0530

    seme more change requared

--- a.py
+++ a.py
@@ -1,4 +1,4 @@
-l=["Akash","Sharma","ankit","Nitin","Upender","Aman","Avinash"]
+l=["Akash","Sharma","ankit","Nitin","Upender","Aman","Avinash","Pankaj"]
 s="a"
 s1="A"
 a=list(filter(lambda x: x if x[0]==s else x[0]==s1,l))
